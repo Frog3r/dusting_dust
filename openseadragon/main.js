8e264d6f063948d5a8dbced3f497534a6ccfaaf7
0xSkyff <danunix0@gmail.com>
2025-10-04 13:13:34 +0200

    tiles added

--- openseadragon/main.js
+++ openseadragon/main.js
@@ -2,15 +2,7 @@ const options =  {
     id: 'container',
     prefixUrl : 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/5.0.1/images/',
     collectionMode : true,
-    tileSources: [
-        {
-            type: 'image',
-            url: 'https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg'
-        },
-        {
-         type: 'image',
-         url: 'https://images.unsplash.com/photo-1517036391698-b004444390fe?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'   
-        }
-    ]
+    tileSources: 'img/pearl.dzi' 
+        
 };2
 const viewer = OpenSeadragon(options);
