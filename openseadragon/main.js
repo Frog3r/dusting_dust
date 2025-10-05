d93b565ec5d07088ef13c65c605cb0e39fa342f7
tonysec <anripo.2006@gmail.com>
2025-10-05 15:05:57 +0200

    Delete openseadragon directory

--- openseadragon/main.js
+++
@@ -1,8 +0,0 @@
-const options =  {
-    id: 'container',
-    prefixUrl : 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/5.0.1/images/',
-    collectionMode : true,
-    tileSources: 'img/pearl.dzi' 
-        
-};2
-const viewer = OpenSeadragon(options);
